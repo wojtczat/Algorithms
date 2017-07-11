b42841253c5ca13b53d74bd4ca0da90c73d4ef16
Tom Wojtczak <wojtczat@mcmaster.ca>
2017-07-11 12:29:00 -0400

    Update LIS.py
    minor changes

--- Dynamic Programming/Longest Increasing Subsequence/LIS.py
+++ Dynamic Programming/Longest Increasing Subsequence/LIS.py
@@ -1,24 +1,15 @@
-# lis returns length of the longest increasing subsequence
-# in arr of size n
+# LIS returns length of the longest increasing subsequence in an arr of size n
 def lis(arr):
     n = len(arr)
-
-    # Declare the list (array) for LIS and initialize LIS
-    # values for all indexes
-    lis = [1]*n
-
+    lis = [1] * n  # Declare the list (array) for LIS and initialize LIS values for all indices
     # Compute optimized LIS values in bottom up manner
     for i in range (1 , n):
         for j in range(0 , i):
             if arr[i] > arr[j] and lis[i]< lis[j] + 1 :
                 lis[i] = lis[j]+1
-
-    # Initialize maximum to 0 to get the maximum of all
-    # LIS
-    maximum = 0
-
+                
     # Pick maximum of all LIS values
+    maximum = 0
     for i in range(n):
         maximum = max(maximum , lis[i])
-
     return maximum
